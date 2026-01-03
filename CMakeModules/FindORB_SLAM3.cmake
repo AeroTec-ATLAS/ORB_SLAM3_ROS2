4ce336a0cc0f3670b35d1a3b0739694584cb249e
Pedro Vicente <pmadcv@gmail.com>
2026-01-03 17:09:04 +0000

    Updated in order to be used on Ubuntu 24.04 LTS with ROS Jazzy

--- CMakeModules/FindORB_SLAM3.cmake
+++ CMakeModules/FindORB_SLAM3.cmake
@@ -1,43 +1,65 @@
-# Try to find ORB_SLAM3
-# Set alternative paths to search for using ORB_SLAM3_DIR
-# Once done this will define
-# You should ensure your ORB_SLAM3 can run correctly
-#
-# To help the search ORB_SLAM3_ROOT_DIR environment variable as the path to ORB_SLAM3 root folder
-#  e.g. `set( ORB_SLAM3_ROOT_DIR=~/ORB_SLAM3) `
-set(ORB_SLAM3_ROOT_DIR "~/Install/ORB_SLAM/ORB_SLAM3")
+# FindORB_SLAM3.cmake
 
-# message(${ORB_SLAM3_ROOT_DIR})
-# message(${ORB_SLAM3_ROOT_DIR}/include)
-# message(${ORB_SLAM3_ROOT_DIR}/Thirdparty/DBoW2/DBoW2)
+set(ORB_SLAM3_ROOT_DIR "$ENV{ORB_SLAM3_ROOT_PATH}/ORB-SLAM3")
 
-# Find ORB_SLAM3
-find_path(ORB_SLAM3_INCLUDE_DIR NAMES System.h
-          PATHS ${ORB_SLAM3_ROOT_DIR}/include)
+# Find ORB_SLAM3 library
+find_library(ORB_SLAM3_LIBRARY
+    NAMES ORB_SLAM3
+    PATHS ${ORB_SLAM3_ROOT_DIR}/lib
+    NO_DEFAULT_PATH
+)
 
-find_library(ORB_SLAM3_LIBRARY NAMES ORB_SLAM3 libORB_SLAM3
-             PATHS ${ORB_SLAM3_ROOT_DIR}/lib)
+# Find ORB_SLAM3 include directory
+find_path(ORB_SLAM3_INCLUDE_DIR
+    NAMES System.h
+    PATHS ${ORB_SLAM3_ROOT_DIR}/include
+    NO_DEFAULT_PATH
+)
 
-# Find built-in DBoW2
-find_path(DBoW2_INCLUDE_DIR NAMES Thirdparty/DBoW2/DBoW2/BowVector.h
-          PATHS ${ORB_SLAM3_ROOT_DIR})
+# Find DBoW2 library
+find_library(DBoW2_LIBRARY
+    NAMES DBoW2
+    PATHS ${ORB_SLAM3_ROOT_DIR}/Thirdparty/DBoW2/lib
+    NO_DEFAULT_PATH
+)
 
-find_library(DBoW2_LIBRARY NAMES DBoW2
-             PATHS ${ORB_SLAM3_ROOT_DIR}/Thirdparty/DBoW2/lib)
+# DBoW2 headers are in the source directory (no separate include dir)
+set(DBoW2_INCLUDE_DIR ${ORB_SLAM3_ROOT_DIR}/Thirdparty/DBoW2)
 
-# Find built-in g2o
-find_library(g2o_LIBRARY NAMES g2o
-             PATHS ${ORB_SLAM3_ROOT_DIR}/Thirdparty/g2o/lib)
-
-
+# Find g2o library
+find_library(g2o_LIBRARY
+    NAMES g2o
+    PATHS ${ORB_SLAM3_ROOT_DIR}/Thirdparty/g2o/lib
+    NO_DEFAULT_PATH
+)
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set ORB_SLAM3_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(ORB_SLAM3  DEFAULT_MSG
-                                  ORB_SLAM3_LIBRARY ORB_SLAM3_INCLUDE_DIR DBoW2_INCLUDE_DIR DBoW2_LIBRARY g2o_LIBRARY)
+find_package_handle_standard_args(ORB_SLAM3
+    REQUIRED_VARS 
+        ORB_SLAM3_LIBRARY 
+        ORB_SLAM3_INCLUDE_DIR 
+        DBoW2_INCLUDE_DIR 
+        DBoW2_LIBRARY 
+        g2o_LIBRARY
+)
 
-mark_as_advanced(ORB_SLAM3_INCLUDE_DIR ORB_SLAM3_LIBRARY )
+if(ORB_SLAM3_FOUND)
+    set(ORB_SLAM3_INCLUDE_DIRS 
+        ${ORB_SLAM3_INCLUDE_DIR}
+        ${ORB_SLAM3_ROOT_DIR}/Thirdparty/DBoW2
+        ${ORB_SLAM3_ROOT_DIR}/Thirdparty/g2o
+    )
+    set(ORB_SLAM3_LIBRARIES 
+        ${ORB_SLAM3_LIBRARY}
+        ${DBoW2_LIBRARY}
+        ${g2o_LIBRARY}
+    )
+endif()
 
-set(ORB_SLAM3_LIBRARIES ${ORB_SLAM3_LIBRARY} ${DBoW2_LIBRARY} ${g2o_LIBRARY})
-set(ORB_SLAM3_INCLUDE_DIRS ${ORB_SLAM3_INCLUDE_DIR} ${DBoW2_INCLUDE_DIR})
+mark_as_advanced(
+    ORB_SLAM3_INCLUDE_DIR
+    ORB_SLAM3_LIBRARY
+    DBoW2_INCLUDE_DIR
+    DBoW2_LIBRARY
+    g2o_LIBRARY
+)
